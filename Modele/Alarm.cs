e220bf5b1dc66c7412453681d980b208db5626e2
KGebski <k.gebski@itmcode.pl>
2022-03-06 16:18:47 +0100

    06.03.2022

---
+++ Modele/Alarm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBenTen.Modele
+{
+    internal abstract class Alarm
+    {
+        string Nazwa { get; set; }
+        bool Enable { get; set; }
+    }
+}
